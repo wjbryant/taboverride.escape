0762dc6d43a0cb60a62dcb6cdaa265842e3f323c
Bill Bryant <bill@wjbryant.com>
2013-07-18 10:58:32 -0400

    use hook system for jQuery adapter

--- src/jquery.taboverride.escape.js
+++ src/jquery.taboverride.escape.js
@@ -41,7 +41,7 @@ Copyright (c) 2013 Bill Bryant | http://opensource.org/licenses/mit */
 
 	$fnTabOverride.escape = tabOverride.escape;
 
-	$fnTabOverride.addDelegatedExtension(function ( $container, selector, enable ) {
+	$fnTabOverride.addExtension( "setDelegated", function ( $container, selector, enable ) {
 		$container.off( ".tabOverrideEscape", selector );
 
 		if ( enable ) {
